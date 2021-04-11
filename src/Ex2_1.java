149a59b08168656494469889a9562f5a958d7d91
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-04-11 18:13:37 +0900

    Initial commit

---
+++ src/Ex2_1.java
@@ -0,0 +1,14 @@
+public class Ex2_1 {
+    public static void main(String args[]) {
+//        sysout 컨트롤 스페이스 자동완성
+        //컨트롤 d >> 행단위 복사
+        //alt shift a 컬럼 편집모드
+        //alt shift 행이동
+        //print() - 출력 후에 줄바꿈을 안함
+        //println() - 출력 후에 줄바꿈을 합니다
+        System.out.println(3+5); //덧셈
+        System.out.println(5-3); //뺄셈
+        System.out.println(3*5); //곱셈
+        System.out.println(3/5); //나눗셈
+    }
+}
