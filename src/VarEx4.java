7b6afdaee32eda3bc7e3145a32b3aeb7ca68fd6c
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-04-12 01:32:07 +0900

    4.11 자바

--- src/VarEx4.java
+++ src/VarEx4.java
@@ -1,13 +1,16 @@
 public class VarEx4 {
     public static void main(String[] args) {
-        //5.두 변수의 값 교환하기
-        int x =10,y=20;
-        int tmp; //빈컵
+//        //5.두 변수의 값 교환하기
+//        int x =10,y=20;
+//        int tmp; //빈컵
+//
+//        tmp =x;
+//        x=y;
+//        y=tmp;
+//        System.out.println("x="+x);
+//        System.out.println("y="+y);
+        //2.값의 타입
+        //값 -문자
 
-        tmp =x;
-        x=y;
-        y=tmp;
-        System.out.println("x="+x);
-        System.out.println("y="+y);
     }
 }
