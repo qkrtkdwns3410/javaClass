3e8052410832e68cd83a48313cdf31153d479ac3
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-04-12 16:01:28 +0900

    4.12

--- src/VarEx7_int_overFlow.java
+++ src/VarEx7_int_overFlow.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 
 public class VarEx7_int_overFlow {
     public static void main(String[] args) {
+    //계수기 (counter)
+        //9999이후 0000이 된다 오버플로우 .. >>표현할 개수를 넘는경우
+        //2진수 0000 0001 >> 1111>>> 0000 오버플로우 발생합니다.
+        //..
+    //2-17 타입간의 변환방법
+        //1.문자와 숫자간의 변환
 
 
 
